1cd193b0a6792ae4ce55245666f545cf82edd872
Ryan Ramaldis <ryanramaldistavares@gmail.com>
2025-12-17 10:16:04 -0300

    script tomando forma

--- automatiza_ronda.py
+++ automatiza_ronda.py
@@ -1,4 +1,5 @@
 import pyautogui
+import time
 
 try:
 
@@ -6,19 +7,28 @@ try:
         x,y = pyautogui.position()
         print(f"{x}, {y}")
 
-    def navega_tela():
-        pyautogui.click(-1226, 834)
-        pyautogui.click(-1813, 18)
-        pyautogui.click(-1710, 336)
-        pyautogui.click(-1203, 774)
+    def abre_navegdor():
+        pyautogui.click(-1593, 838)
+        time.sleep(2)
+        pyautogui.doubleClick(697, 669)
+        time.sleep(2)
+        pyautogui.click(-1676, 101)
+        time.sleep(5)
 
-    def escreve_tela()
+    def abre_conversa():
+        pyautogui.click(-1580, 350)
+
+    def escreve_tela():
+        pyautogui.click(-1102, 783)
+        pyautogui.write('teste')
 
 
     if __name__ == "__main__":
 
         captura_coord()
-        navega_tela()
+        abre_navegdor()
+        abre_conversa()
+        escreve_tela()
 
 except:
 
